2bfcd96c70091705769630b53b52900ba84dc247
Sandeep Singh <singh.deep8302@gmail.com>
2023-10-11 02:53:43 -0400

    querys for searching for movies

--- server.js
+++ server.js
@@ -59,7 +59,6 @@ app.get('/customers', (req, res) => {
 
 app.post('/customername', (req, res) => {
   const name = req.body.name;
-  console.log(name)
   con.query(`Select * from Customer Where customer.first_name Like "%${name}%" OR customer.last_name Like "%${name}%";`, (err, rows) => {
     if (err) throw err
     res.json(rows)
@@ -69,8 +68,31 @@ app.post('/customername', (req, res) => {
 
 app.post('/customerid', (req, res) => {
   const id = req.body.id;
-  console.log(id)
   con.query(`Select * from Customer Where customer.customer_id = ${id};`, (err, rows) => {
+    if (err) throw err
+    res.json(rows)
+  })
+})
+
+app.post('/actornamemovie', (req, res) => {
+  const actorname = req.body.actorname;
+  con.query(`SELECT film.title FROM film JOIN film_actor ON film.film_id = film_actor.film_id JOIN actor ON film_actor.actor_id = actor.actor_id WHERE actor.first_name Like "%${actorname}%" OR actor.last_name Like "%${actorname}%";`, (err, rows) => {
+    if (err) throw err
+    res.json(rows)
+  })
+})
+
+app.post('/movienamemovie', (req, res) => {
+  const moviename = req.body.moviename;
+  con.query(`SELECT title FROM film WHERE title LIKE "%${moviename}%";`, (err, rows) => {
+    if (err) throw err
+    res.json(rows)
+  })
+})
+
+app.post('/genremovie', (req, res) => {
+  const genre = req.body.genre;
+  con.query(`SELECT film.title FROM film JOIN film_category ON film.film_id = film_category.film_id JOIN category ON film_category.category_id = category.category_id WHERE category.name LIKE "%${genre}%";`, (err, rows) => {
     if (err) throw err
     res.json(rows)
   })
